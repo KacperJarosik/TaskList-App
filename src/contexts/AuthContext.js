a8666c3f7a988c496a5ccba7ffd29f82a07b1df7
OlaPindral <pindral.ola@gmail.com>
2024-05-12 18:13:48 +0200

    Implementacja funkcji rejestracji logowania i zmiany hasła

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,27 +1,72 @@
 import React, { useContext, useState, useEffect } from "react";
-import { auth } from "../firebase";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, onAuthStateChanged } from "firebase/auth";
+import {firestore} from "../firebase";
+import { collection, doc, setDoc } from "firebase/firestore";
+
 const AuthContext = React.createContext();
 
 export function useAuth() {
-  useContext(AuthContext);
+  return useContext(AuthContext);
 }
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
 
-  function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password);
+  const authInstance = getAuth();
+  const db = firestore;
+
+function signup(email, password,name) {
+    // if(!email.endsWith("@edu.p.lodz.pl")){
+    //   throw new Error("Adress e-mail musi zawierać domenę @edu.p.lodz.pl");
+    // }
+
+    return createUserWithEmailAndPassword(authInstance,email, password)
+    .then((userCredential)=>{
+      const user = userCredential.user;
+      return setDoc(doc(collection(db,"users"),user.uid),{
+        name: name,
+        email: email
+      });
+    })
+    .catch(error => {
+      console.error("Error during signup: ", error);
+      throw error;
+    })
   }
+
+  function login(email,password){
+    return signInWithEmailAndPassword(authInstance, email, password)
+      .catch(error=>{
+        console.error("Error during login: ", error);
+        throw error;
+      })
+  }
+
+  function logout(){
+    return authInstance.signOut();
+  }
+
+  function resetPassword(email){
+    return sendPasswordResetEmail(authInstance,email)
+    .catch((error)=>{
+      console.error("Error during password reset: ", error);
+      throw error;
+    });
+  }
+
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
+    const unsubscribe = onAuthStateChanged(authInstance,(user) => {
       setCurrentUser(user);
     });
     return unsubscribe;
-  }, []);
+  }, [authInstance]);
 
   const value = {
     currentUser,
     signup,
+    login,
+    logout,
+    resetPassword,
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
