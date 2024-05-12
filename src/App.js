d7bbe943d695137de50a3c180f0757a78fce60fe
OlaPindral <pindral.ola@gmail.com>
2024-05-12 18:08:13 +0200

    importowanie AuthProvider do funkcji App  i aktualizacja zależności

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import { AfterLogin } from './Components/LoginSignup/AfterLogin';
+import { AuthProvider } from './contexts/AuthContext';
 function App() {
   return (
   <div>
-<Routes>
-  <Route path='/' element={<LoginSignup/>}/>
-  <Route path='/after' element={<AfterLogin/>}/>
-</Routes>
-
+    <AuthProvider>
+      <Routes>
+        <Route path='/' element={<LoginSignup/>}/>
+        <Route path='/after' element={<AfterLogin/>}/>
+      </Routes>
+    </AuthProvider>
   </div>
   );
 }
