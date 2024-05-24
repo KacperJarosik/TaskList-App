4880994cbe8a72bce53a20462318e55313c21260
235909 <245826@edu.p.lodz.pl>
2024-05-24 08:46:21 +0200

    Przypisanie dodawania domyślnych zadań w kategoriach

--- src/Components/MainView/TasksList.tsx
+++ src/Components/MainView/TasksList.tsx
@@ -1,4 +1,4 @@
-//import React from 'react';
+import { useState } from 'react';
 import TaskManager from '../../Classes.js';
 
 // Load data from storage
@@ -7,13 +7,21 @@ TaskManager.loadFromStorage();
 // Get categories from the TaskManager
 const categories = TaskManager.categories;
 
-function TasksList({tasks}) {
+function TasksList({ tasks, categoryId }) {
+    const [taskList, setTaskList] = useState(tasks); // State to manage tasks
+    //na razie dodaje 1 max element zobaczymy jak zadziała z bazą (czasem doda więcej po jakimś czasie)
+    const handleAddTask = () => {
+        TaskManager.addTask(categoryId, '-', '-');
+        const updatedTasks = TaskManager.categories.find(cat => cat.id === categoryId).tasks;
+        setTaskList(updatedTasks);
+    };
+
     return (
         <>
             <div className="buttons">
                 <button className="SearchButton" type="submit">Wyszukaj</button>
                 <button className="FilteringButton" type="submit">Filtruj</button>
-                <button className="AddTaskButton" type="submit">Dodaj</button>
+                <button className="AddTaskButton" type="button" onClick={handleAddTask}>Dodaj</button>
             </div>
             <table className="TasksListTable">
                 <thead>
@@ -26,7 +34,7 @@ function TasksList({tasks}) {
                 </tr>
                 </thead>
                 <tbody>
-                {tasks.map(task => (
+                {taskList.map(task => (
                     <tr key={task.id}>
                         <td>{task.category}</td>
                         <td>{task.text}</td>
@@ -34,10 +42,10 @@ function TasksList({tasks}) {
                         <td>{task.status}</td>
                         <td className="TaskDetails">
                             <span className="DetailsText">Szczegóły</span>
-                            {/*Do zrobienia jako wyświetlanie szczegółów po kliknięciu w "Szczegóły"*/}
-                            {/*Jeśli istnieje potrzeba sprawdzenia poprawności szczegółów to:*/}
-                            {/*ująć powyższe w komentarz, a poniższe odkomentować*/}
-                            {/*<i>{task.details}</i>*/}
+                            {/* Do zrobienia jako wyświetlanie szczegółów po kliknięciu w "Szczegóły" */}
+                            {/* Jeśli istnieje potrzeba sprawdzenia poprawności szczegółów to: */}
+                            {/* ująć powyższe w komentarz, a poniższe odkomentować */}
+                            {/* <i>{task.details}</i> */}
                             <button className="DeleteTaskButton" type="submit">Usuń</button>
                             <button className="EditTaskButton" type="submit">Edytuj</button>
                         </td>
@@ -63,7 +71,7 @@ function CategoriesList() {
                         status: task.status,
                         details: task.details,
                         category: category.title // Add category title to each task
-                    }))}/>
+                    }))} categoryId={category.id} />
                 </div>
             ))}
         </>
