f6516b2aa66b33025de70d26fdb4aa68c91d8c85
235909 <245826@edu.p.lodz.pl>
2024-05-27 16:06:37 +0200

    Dodanie działającego wylogowania

--- src/Components/MainView/LogOut.tsx
+++ src/Components/MainView/LogOut.tsx
@@ -1,9 +1,21 @@
+// LogOut.tsx
+//import React from 'react';
+import { useNavigate } from 'react-router-dom';
 // @ts-ignore
-import logout from "../Assets/wylogowanie.png"
+import logout from "../Assets/wylogowanie.png";
 
 function LogOut() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // Clear user data here, e.g., localStorage or session
+        localStorage.removeItem('userToken'); // Example, depends on how you store user data
+        // Redirect to login/signup page
+        navigate('../');
+    };
+
     return (
-        <div>
+        <div onClick={handleLogout} className="LogOutContainer">
             <img src={logout} alt="Wylogowanie" className="LogOutIcon"/>
             <p>Wyloguj się</p>
         </div>
