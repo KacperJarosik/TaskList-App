34d8f0638c587570e7476470b9b54e6c3fe73601
235909 <245826@edu.p.lodz.pl>
2024-06-02 18:19:01 +0200

    Dodanie czyszczenia wiadomości po naciśnięciu wyślij

--- src/Tabs/OpinionsViev.jsx
+++ src/Tabs/OpinionsViev.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import '../Components/LoginSignup/AfterLogin.css';
+import React, { useState } from 'react';
 
 const OpinionsViev = () => {
+    const [opinion, setOpinion] = useState('');
 
-  return (
-    <div className='back'>
-      <h3 className="back">Podziel się z nami swoją opinią</h3>
-      <form className="opinion-form">
-        <input 
-          type="text"  
-          className="opinion-input"
-        />
-        <div className='submit-button'>Wyśłij</div>
-      </form>
-    </div>
-  );
+    const handleSendClick = () => {
+        setOpinion('');
+    };
+
+    return (
+        <div className='back'>
+            <h3 className="back">Podziel się z nami swoją opinią</h3>
+            <form className="opinion-form">
+                <input
+                    type="text"
+                    className="opinion-input"
+                    value={opinion}
+                    onChange={(e) => setOpinion(e.target.value)}
+                />
+                <button
+                    type="button"
+                    className="AddButton"
+                    onClick={handleSendClick}
+                >
+                    Wyślij
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default OpinionsViev;
