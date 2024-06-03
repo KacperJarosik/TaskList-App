4f5952e688f0589594687b2a0fe28b39847845d9
235909 <245826@edu.p.lodz.pl>
2024-06-03 10:39:58 +0200

    Ponowne wyświetlanie ikon opcji

--- src/Components/MainView/NavigationBar.tsx
+++ src/Components/MainView/NavigationBar.tsx
@@ -1,6 +1,15 @@
 //import React from "react";
 import { useNavigate } from "react-router-dom/dist";
-
+// @ts-ignore
+import mainView from "../Assets/panel_glowny.png";
+// @ts-ignore
+import categories from "../Assets/kategorie.png";
+// @ts-ignore
+import groups from "../Assets/grupy.png";
+// @ts-ignore
+import opinions from "../Assets/opinie.png";
+// @ts-ignore
+import settings from "../Assets/ustawienia.png";
 function NavigationBar() {
     const navigate = useNavigate();
 
@@ -26,11 +35,17 @@ function NavigationBar() {
 
     return (
         <ul className="list-group">
-            <li className="list-group-item" onClick={handleMainVievClick}>Panel główny</li>
-            <li className="list-group-item" onClick={handleCategoriesClick}>Kategorie</li>
-            {/* <li className="list-group-item" onClick={handleGroupsClick}>Grupy</li> */}
-            <li className="list-group-item" onClick={handleOpinionsClick}>Wyślij opinię</li>
-            <li className="list-group-item" onClick={handleSettingsClick}>Ustawienia</li>
+            <li className="list-group-item" onClick={handleMainVievClick}><img src={mainView} alt="Panel główny" className="NavigationBarIcon"/>Panel
+                główny
+            </li>
+            <li className="list-group-item" onClick={handleCategoriesClick}><img src={categories} alt="Kategorie" className="NavigationBarIcon"/>Kategorie
+            </li>
+            {/* <li className="list-group-item" onClick={handleGroupsClick}><img src={groups} alt="Grupy" className="NavigationBarIcon"/>Grupy</li> */}
+            <li className="list-group-item" onClick={handleOpinionsClick}><img src={opinions} alt="Opinie" className="NavigationBarIcon"/>Wyślij
+                opinię
+            </li>
+            <li className="list-group-item" onClick={handleSettingsClick}><img src={settings} alt="Ustawienia" className="NavigationBarIcon"/>Ustawienia
+            </li>
         </ul>
     );
 }
