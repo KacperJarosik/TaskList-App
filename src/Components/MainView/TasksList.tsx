05bc17f56f541b74735c686c3565769094ef5ad6
Kamil Kaźmierczak <kamilkazmierczak@MacBook-Air-Kamil.local>
2024-06-16 17:22:11 +0200

    Dołączenie opracowanych komentarzy i refaktoryzacja plików na gałęzi master

--- src/Components/MainView/TasksList.tsx
+++ src/Components/MainView/TasksList.tsx
@@ -1,5 +1,6 @@
-import { useState, useEffect, useRef } from 'react';
+import {useState, useEffect, useRef} from 'react';
 import TaskManager from '../../Structs/TaskManager.js';
+//  Importing icons
 // @ts-ignore
 import arrow_right from "../Assets/strzalka_prawo.png";
 // @ts-ignore
@@ -10,27 +11,57 @@ TaskManager.loadFromStorage();
 // Get categories from the TaskManager
 const categories = TaskManager.categories;
 
-function TasksList({ tasks, categoryId }) {
+function TasksList({tasks, categoryId}) {
     const [taskList, setTaskList] = useState(tasks); // State to manage tasks
-    const [isSearchInputVisible, setIsSearchInputVisible] = useState(false); // State to manage search input visibility
-    const searchInputRef = useRef(null); // Reference to the search input
+    const [isSearchInputVisible, setIsSearchInputVisible] = useState(false);    // State to manage search input visibility
+    const searchInputRef = useRef(null);    // Reference to the search input
     const [searchQuery, setSearchQuery] = useState(''); // State to store search query
     const [showDetails, setShowDetails] = useState(false);
-    const [currentTask, setCurrentTask] = useState(null);
-    const [isEditing, setIsEditing] = useState(false);
-    const [editingTask, setEditingTask] = useState(null);
-    const [isFiltering, setIsFiltering] = useState(false);
-    const [isSorting, setIsSorting] = useState(false);
-    const [isAdding, setIsAdding] = useState(false);
-    const [taskName, setTaskName] = useState('');
+    const [currentTask, setCurrentTask] = useState(null);   // Variable storing task data
+    const [isEditing, setIsEditing] = useState(false);  // Flag to check if data is editing
+    const [editingTask, setEditingTask] = useState(null);   // Variable storing task data
+    const [isFiltering, setIsFiltering] = useState(false);  // Flag to check if data is filtering
+    const [isSorting, setIsSorting] = useState(false);  // Flag to check if data is sorting
+    const [isAdding, setIsAdding] = useState(false);    // Flag to check if data is adding
+    const [taskName, setTaskName] = useState('');   // Tasks data
     const [taskDate, setTaskDate] = useState('');
     const [taskDetails, setTaskDetails] = useState('');
     const [sortOption, setSortOption] = useState('dateASC'); // Set default sort option
-
-    // States for filtering
-    const [filterStartDate, setFilterStartDate] = useState('');
+    const [filterStartDate, setFilterStartDate] = useState(''); // Filtering by deadline date
     const [filterEndDate, setFilterEndDate] = useState('');
-    const [filterStatus, setFilterStatus] = useState('');
+    const [filterStatus, setFilterStatus] = useState('');   // Filtering by task status
+
+    // Handling a click action on search button
+    const handleSearchButtonClick = () => {
+        setIsSearchInputVisible(true);  // Show search input
+    };
+
+    // Handling a clicking outside of search input
+    const handleClickOutside = (event) => {
+        if (searchInputRef.current && !searchInputRef.current.contains(event.target)) {
+            setIsSearchInputVisible(false); // Hide search input
+        }
+    };
+
+    // Handling a change of searching input
+    const handleSearchInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    // Handling a change of search input visibility
+    useEffect(() => {
+        if (isSearchInputVisible) {
+            document.addEventListener('mousedown', handleClickOutside);
+            if (searchInputRef.current) {
+                searchInputRef.current.focus(); // Focus on the search input
+            }
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isSearchInputVisible]);
 
     // Adding a task
     const handleAddTask = () => {
@@ -52,70 +83,48 @@ function TasksList({ tasks, categoryId }) {
         setIsEditing(false); // Close the editing mode
     };
 
+    // Deleting a task
     const handleDeleteTask = (taskId) => {
         TaskManager.removeTask(categoryId, taskId);
         const updatedTasks = TaskManager.categories.find(cat => cat.id === categoryId).tasks;
         setTaskList([...updatedTasks]); // Update the task list state
     };
 
-    const handleSearchButtonClick = () => {
-        setIsSearchInputVisible(true); // Show search input
-    };
-
-    const handleClickOutside = (event) => {
-        if (searchInputRef.current && !searchInputRef.current.contains(event.target)) {
-            setIsSearchInputVisible(false); // Hide search input
-        }
-    };
-
-    useEffect(() => {
-        if (isSearchInputVisible) {
-            document.addEventListener('mousedown', handleClickOutside);
-            if (searchInputRef.current) {
-                searchInputRef.current.focus(); // Focus on the search input
-            }
-        } else {
-            document.removeEventListener('mousedown', handleClickOutside);
-        }
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [isSearchInputVisible]);
-
-    const handleSearchInputChange = (event) => {
-        setSearchQuery(event.target.value);
-    };
-
     const filteredTasks = taskList.filter(task =>
         task.text.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
-    const handleDetailsClick = (task) => {
-        setCurrentTask(task);
-        setShowDetails(true);
-    };
-
-    const handleEditClick = (task) => {
-        setEditingTask(task);
-        setIsEditing(true);
-    };
-
+    // Handling a click action on filtering button
     const handleFilteringClick = () => {
         setIsFiltering(true);
     };
 
+    // Handling apply of filtering changes
+    const applyFiltering = () => {
+        const updatedTasks = tasks.filter(task => {
+            const taskDate = new Date(task.date);
+            const startDate = filterStartDate ? new Date(filterStartDate) : null;
+            const endDate = filterEndDate ? new Date(filterEndDate) : null;
+            const matchesStatus = filterStatus ? task.status === filterStatus : true;
+            const matchesStartDate = startDate ? taskDate >= startDate : true;
+            const matchesEndDate = endDate ? taskDate <= endDate : true;
+            return matchesStatus && matchesStartDate && matchesEndDate;
+        });
+        setTaskList(updatedTasks);
+        setIsFiltering(false); // Close the filtering mode
+    };
+
+    // Handling a click action on sorting button
     const handleSortingClick = () => {
         setIsSorting(true);
     };
 
-    const handleAddClick = () => {
-        setIsAdding(true);
-    };
-
+    // Handling a change of options in sorting popup
     const handleSortOptionChange = (event) => {
         setSortOption(event.target.value);
     };
 
+    // Handling apply of sorting changes
     const applySorting = () => {
         const updatedTasks = [...taskList];
         switch (sortOption) {
@@ -138,20 +147,24 @@ function TasksList({ tasks, categoryId }) {
         setIsSorting(false); // Close the sorting mode
     };
 
-    const applyFiltering = () => {
-        const updatedTasks = tasks.filter(task => {
-            const taskDate = new Date(task.date);
-            const startDate = filterStartDate ? new Date(filterStartDate) : null;
-            const endDate = filterEndDate ? new Date(filterEndDate) : null;
-            const matchesStatus = filterStatus ? task.status === filterStatus : true;
-            const matchesStartDate = startDate ? taskDate >= startDate : true;
-            const matchesEndDate = endDate ? taskDate <= endDate : true;
-            return matchesStatus && matchesStartDate && matchesEndDate;
-        });
-        setTaskList(updatedTasks);
-        setIsFiltering(false); // Close the filtering mode
+    // Handling a click action on details button
+    const handleDetailsClick = (task) => {
+        setCurrentTask(task);
+        setShowDetails(true);
     };
 
+    // Handling a click action on edit button
+    const handleEditClick = (task) => {
+        setEditingTask(task);
+        setIsEditing(true);
+    };
+
+    // Handling a click action on add button
+    const handleAddClick = () => {
+        setIsAdding(true);
+    };
+
+    // Displaying tasks list with categories
     return (
         <>
             <div className="taskButtons">
@@ -159,7 +172,8 @@ function TasksList({ tasks, categoryId }) {
                     <button className="SearchButton" type="button" onClick={handleSearchButtonClick}>Wyszukaj</button>
                 )}
                 {isSearchInputVisible && (
-                    <input ref={searchInputRef} type="text" className="SearchInput" value={searchQuery} onChange={handleSearchInputChange} />
+                    <input ref={searchInputRef} type="text" className="SearchInput" value={searchQuery}
+                           onChange={handleSearchInputChange}/>
                 )}
                 <button className="FilteringButton" onClick={handleFilteringClick}>Filtruj</button>
                 <button className="SortingButton" onClick={handleSortingClick}>Sortuj</button>
@@ -168,21 +182,23 @@ function TasksList({ tasks, categoryId }) {
             <table className="TasksListTable">
                 <thead>
                 <tr>
-                    <th className="TaskName">Nazwa</th>
-                    <th className="TaskDeadline">Termin</th>
-                    <th className="TaskStatus">Status</th>
-                    <th className="TaskDetails">Szczegóły</th>
+                    <th className="Table_Name">Nazwa</th>
+                    <th className="Table_Deadline">Termin</th>
+                    <th className="Table_Status">Status</th>
+                    <th className="Table_Details">Szczegóły</th>
                 </tr>
                 </thead>
                 <tbody>
                 {filteredTasks.map(task => (
                     <tr key={task.id}>
-                        <td className="TaskName">{task.text}</td>
-                        <td className="TaskDeadline">{task.date}</td>
-                        <td className="TaskStatus">{task.status}</td>
-                        <td className="TaskDetails">
+                        <td className="Table_Name">{task.text}</td>
+                        <td className="Table_Deadline">{task.date}</td>
+                        <td className="Table_Status">{task.status}</td>
+                        <td className="Table_Details">
                             <span onClick={() => handleDetailsClick(task)}>Szczegóły</span>
-                            <button className="DeleteButton" type="button" onClick={() => handleDeleteTask(task.id)}>Usuń</button>
+                            <button className="DeleteButton" type="button"
+                                    onClick={() => handleDeleteTask(task.id)}>Usuń
+                            </button>
                             <button className="EditButton" onClick={() => handleEditClick(task)}>Edytuj</button>
                         </td>
                     </tr>
@@ -205,15 +221,18 @@ function TasksList({ tasks, categoryId }) {
                     <h3>Edycja zadania</h3>
                     <label>
                         Nazwa:
-                        <input type="text" value={editingTask.text} onChange={(e) => setEditingTask({ ...editingTask, text: e.target.value })} />
+                        <input type="text" value={editingTask.text}
+                               onChange={(e) => setEditingTask({...editingTask, text: e.target.value})}/>
                     </label>
                     <label>
                         Data:
-                        <input type="date" value={editingTask.date} onChange={(e) => setEditingTask({ ...editingTask, date: e.target.value })} />
+                        <input type="date" value={editingTask.date}
+                               onChange={(e) => setEditingTask({...editingTask, date: e.target.value})}/>
                     </label>
                     <label>
                         Status:
-                        <select value={editingTask.status} onChange={(e) => setEditingTask({ ...editingTask, status: e.target.value })}>
+                        <select value={editingTask.status}
+                                onChange={(e) => setEditingTask({...editingTask, status: e.target.value})}>
                             <option value="Do zrobienia">Do zrobienia</option>
                             <option value="W trakcie">W trakcie</option>
                             <option value="Zakończone">Zakończone</option>
@@ -221,7 +240,8 @@ function TasksList({ tasks, categoryId }) {
                     </label>
                     <label>
                         Szczegóły:
-                        <textarea value={editingTask.details} onChange={(e) => setEditingTask({ ...editingTask, details: e.target.value })} />
+                        <textarea value={editingTask.details}
+                                  onChange={(e) => setEditingTask({...editingTask, details: e.target.value})}/>
                     </label>
                     <div className="buttons-container">
                         <button onClick={handleSaveEditTask}>Save</button>
@@ -235,11 +255,12 @@ function TasksList({ tasks, categoryId }) {
                     <h3>Filtruj zadania</h3>
                     <div>
                         Data od:
-                        <input type="date" value={filterStartDate} onChange={(e) => setFilterStartDate(e.target.value)} />
+                        <input type="date" value={filterStartDate}
+                               onChange={(e) => setFilterStartDate(e.target.value)}/>
                     </div>
                     <div>
                         Data do:
-                        <input type="date" value={filterEndDate} onChange={(e) => setFilterEndDate(e.target.value)} />
+                        <input type="date" value={filterEndDate} onChange={(e) => setFilterEndDate(e.target.value)}/>
                     </div>
                     <div>
                         Status:
@@ -262,16 +283,16 @@ function TasksList({ tasks, categoryId }) {
                     <h3>Sortuj zadania</h3>
                     <div className="sortByContainer" onChange={handleSortOptionChange}>
                         <label>
-                            <input type="radio" name="sort" value="dateASC" checked={sortOption === 'dateASC'} /><span> ⭡ wg terminu</span>
+                            <input type="radio" name="sort" value="dateASC" checked={sortOption === 'dateASC'}/><span> ⭡ wg terminu</span>
                         </label>
                         <label>
-                            <input type="radio" name="sort" value="dateDESC" checked={sortOption === 'dateDESC'} /><span> ⭣ wg terminu</span>
+                            <input type="radio" name="sort" value="dateDESC" checked={sortOption === 'dateDESC'}/><span> ⭣ wg terminu</span>
                         </label>
                         <label>
-                            <input type="radio" name="sort" value="nameASC" checked={sortOption === 'nameASC'} /><span> ⭡ wg nazwy</span>
+                            <input type="radio" name="sort" value="nameASC" checked={sortOption === 'nameASC'}/><span> ⭡ wg nazwy</span>
                         </label>
                         <label>
-                            <input type="radio" name="sort" value="nameDESC" checked={sortOption === 'nameDESC'} /><span> ⭣ wg nazwy</span>
+                            <input type="radio" name="sort" value="nameDESC" checked={sortOption === 'nameDESC'}/><span> ⭣ wg nazwy</span>
                         </label>
                     </div>
                     <div className="buttons-container">
@@ -285,15 +306,17 @@ function TasksList({ tasks, categoryId }) {
                     <h3>Dodaj zadanie</h3>
                     <label>
                         Nazwa:
-                        <input type="text" placeholder="Nazwa zadania..." value={taskName} onChange={(e) => setTaskName(e.target.value)} />
+                        <input type="text" placeholder="Nazwa zadania..." value={taskName}
+                               onChange={(e) => setTaskName(e.target.value)}/>
                     </label>
                     <label>
                         Data:
-                        <input type="date" value={taskDate} onChange={(e) => setTaskDate(e.target.value)} />
+                        <input type="date" value={taskDate} onChange={(e) => setTaskDate(e.target.value)}/>
                     </label>
                     <label>
                         Szczegóły:
-                        <textarea placeholder="Szczegóły zadania..." value={taskDetails} onChange={(e) => setTaskDetails(e.target.value)} />
+                        <textarea placeholder="Szczegóły zadania..." value={taskDetails}
+                                  onChange={(e) => setTaskDetails(e.target.value)}/>
                     </label>
                     <div className="buttons-container">
                         <button onClick={handleAddTask}>Dodaj</button>
@@ -308,6 +331,7 @@ function TasksList({ tasks, categoryId }) {
 function CategoriesList() {
     const [visibleCategories, setVisibleCategories] = useState({});
 
+    // Toggling visibility when click on category name
     const toggleCategoryVisibility = (categoryId) => {
         setVisibleCategories(prevState => ({
             ...prevState,
@@ -315,6 +339,7 @@ function CategoriesList() {
         }));
     };
 
+    // Displaying category's tasks
     return (
         <>
             <h3>Zadania do wykonania</h3>
@@ -335,7 +360,7 @@ function CategoriesList() {
                                     task.status === 'Done' ? 'Zakończone' : task.status,
                             details: task.details,
                             category: category.title // Add category title to each task
-                        }))} categoryId={category.id} />
+                        }))} categoryId={category.id}/>
                     )}
                 </div>
             ))}
